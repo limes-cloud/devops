f6d4df18cb1f6116330f9de45dbc9628b58584e2
lime <1280291001@qq.com>
2022-04-01 01:24:28 +0800

    初始化架构

---
+++ api/internal/logic/getUser.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"configure/api/internal/svc"
+	"configure/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetUserLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLogic {
+	return &GetUserLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GetUserLogic) GetUser() (resp *types.GetUserResponse, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
